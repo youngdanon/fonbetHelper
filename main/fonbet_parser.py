a231f7cdcb86afe8e3fbb4c2cb29583e87f2adc7
youngdanon <far.danil@outlook.com>
2021-11-22 21:44:07 +0300

    refactored main/apps.py

--- main/fonbet_parser.py
+++ main/fonbet_parser.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 
 class EventsParser:
-    def __init__(self):
+    def __init__(self, current_version=0):
         self.session = requests.Session()
         self.session.headers = settings.FONBET_SESSION_HEADERS
-        self.current_version = 0
+        self.current_version = current_version
 
     def get_updates(self):
         params = {'lang': 'ru',
@@ -25,21 +25,3 @@ class EventsParser:
         response = self.session.get(url)
         response.raise_for_status()
         return response.json()['serverTime']
-
-    @staticmethod
-    def initial_dump(updates):
-        sport_kinds = []
-        sport_segments = []
-        for sport in updates['sports']:
-            if sport['kind'] == 'sport':
-                sport_kinds.append(sport)
-            elif sport['kind'] == 'segment':
-                sport_segments.append(sport)
-        EventsParser.dump(sport_kinds, 'json/sport_kinds.json')
-        EventsParser.dump(sport_segments, 'json/sport_segments.json')
-        EventsParser.dump(updates['events'], 'json/events.json')
-
-    @staticmethod
-    def dump(json_obj, path):
-        with open(path, 'w', encoding='utf-8') as file:
-            json.dump(json_obj, file, indent=2, ensure_ascii=False)
---
+++ main/server_startup.py
@@ -0,0 +1,135 @@
+import logging
+import os
+import coloredlogs
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+coloredlogs.install(logger=logger)
+
+
+def init_db_clean(SportKind, SportSegment, Event, EventSegment, Factor):
+    SportKind.objects.all().delete()
+    SportSegment.objects.all().delete()
+    Event.objects.all().delete()
+    EventSegment.objects.all().delete()
+    Factor.objects.all().delete()
+    logger.info("DB cleanup complete")
+
+
+def init_sports(sports_data: list, SportKind, SportSegment):
+    for sport in sports_data:
+        if sport.get('kind') == 'sport':
+            SportKind(fonkey=sport.get('id'), name=sport.get('name')).save()
+        else:
+            sport_kind = SportKind.objects.get(fonkey=sport.get('parentId'))
+            SportSegment(fonkey=sport.get('id'), name=sport.get('name'), sport_kind=sport_kind).save()
+    logger.info("SportKind/segment models initialization complete")
+
+
+def init_events(events_data: list, Event, EventSegment, SportSegment):
+    for event in events_data:
+        if event.get('name'):
+            event_name = event.get('name')
+        else:
+            event_name = f"{event.get('team1', '')} - {event.get('team2', '')}"
+        if event.get('level') == 1:
+            event_ss = SportSegment.objects.get(fonkey=event.get('sportId'))
+            event_sk = event_ss.sport_kind
+            event_fullname = f"{event_sk.name} / {event_ss.name} / {event_name}"
+            event_url = f"https://www.fonbet.ru/sports/{event_sk.fonkey}/{event_ss.fonkey}/{event.get('id')}"
+            Event(fonkey=event.get('id'),
+                  name=event_name,
+                  full_name=event_fullname,
+                  url=event_url,
+                  is_live=event.get('place') == 'live',
+                  start_time=event.get('startTime'),
+                  sport_segment=event_ss
+                  ).save()
+        elif event.get('level') == 2:
+            # print(event.get('level'), event.get('parentId'))
+            parent_event = Event.objects.get(fonkey=event.get('parentId'))
+            EventSegment(fonkey=event.get('id'),
+                         name=event_name,
+                         start_time=event.get('startTime'),
+                         event=parent_event,
+                         is_live=event.get('place') == 'live'
+                         ).save()
+        else:
+            EventSegment(fonkey=event.get('id'),
+                         name=event_name,
+                         start_time=event.get('startTime'),
+                         parent_id=event.get('parentId'),
+                         is_live=event.get('place') == 'live'
+                         ).save()
+    logger.info("Events/EventSegments models initialization complete")
+
+
+def init_event_miscs(events_miscs: list, Event, EventSegment):
+    for misc in events_miscs:
+        current_event = Event.objects.filter(fonkey=misc.get('id')).first()
+        if not current_event:
+            current_event = EventSegment.objects.filter(fonkey=misc.get('id')).first()
+        current_event.score_comment = misc.get('comment')
+        current_event.score1 = misc.get('score1')
+        current_event.score2 = misc.get('score2')
+        current_event.save()
+    logger.info("Miscs initialization complete")
+
+
+def init_factors(factors: list, Event, EventSegment, Factor):
+    for event in factors:
+        is_event = True
+        current_event = Event.objects.filter(fonkey=event.get('e')).first()
+        if not current_event:
+            is_event = False
+            current_event = EventSegment.objects.filter(fonkey=event.get('e')).first()
+        for factor in event.get('factors'):
+            if is_event:
+                Factor(fonkey=factor.get('f'),
+                       param=factor.get('p'),
+                       value=factor.get('v'),
+                       event=current_event).save()
+            else:
+                Factor(fonkey=factor.get('f'),
+                       param=factor.get('p'),
+                       value=factor.get('v'),
+                       event_segment=current_event).save()
+    logger.info("Factors initialization complete")
+
+
+def init_event_blocks(event_blocks: list, Event, EventSegment, Factor):
+    for block in event_blocks:
+        current_event = Event.objects.filter(fonkey=block.get('eventId')).first()
+        if not current_event:
+            current_event = EventSegment.objects.filter(fonkey=block.get('eventId')).first()
+        if block.get('state') == 'blocked':
+            current_event.is_blocked = True
+        elif block.get('state') == 'unblocked':
+            current_event.is_blocked = False
+        elif block.get('state') == 'partial':
+            for factor_fonkey in block.get('factors'):
+                # print(factor_fonkey)
+                # print(current_event.factors.all())
+                factor = current_event.factors.filter(fonkey=factor_fonkey).first()
+                print(factor)
+                if not factor:
+                    continue
+                factor.is_blocked = True
+                factor.save()
+        current_event.save()
+    logger.info("Event blocks initialization complete")
+
+
+def on_startup():
+    from .models import SportKind, SportSegment, Event, EventSegment, Factor
+    from .fonbet_parser import EventsParser
+    init_db_clean(SportKind, SportSegment, Event, EventSegment, Factor)
+    parser = EventsParser()
+    initial_updates = parser.get_updates()
+    os.environ['current_version'] = str(parser.current_version)
+    init_sports(initial_updates.get('sports'), SportKind, SportSegment)
+    init_events(initial_updates.get('events'), Event, EventSegment, SportSegment)
+    init_event_miscs(initial_updates.get('eventMiscs'), Event, EventSegment)
+    init_factors(initial_updates.get('customFactors'), Event, EventSegment, Factor)
+    init_event_blocks(initial_updates.get('eventBlocks'), Event, EventSegment, Factor)
+    logger.info("All models initialization complete")
